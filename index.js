94bd2098e75e6cd0f877d7ee1ccf7cbd8d9f8a66
Rob Scherer <robscherer6@gmail.com>
2022-08-15 09:18:38 -0400

    validating input function

--- index.js
+++ index.js
@@ -4,38 +4,72 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound (playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  console.log('Player: ', playerSelection);
-  let playerScore = 0;
-  computerSelection = computerSelection.toLowerCase();
-  console.log('Computer: ', computerSelection);
-  let computerScore = 0;
-  if (playerSelection === 'rock' && computerSelection === 'paper') {
-    computerScore++;
-  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    computerScore++;
-  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    computerScore++;
-  } else if (playerSelection === computerSelection) {
-
-  } else {
-    playerScore++;
+function playerChoice () {
+  let input = prompt('Type Rock, Paper, or Scissors');
+  while (input === null) {
+    input = prompt('Type Rock, Paper, or Scissors');
   }
-  console.log('PlayerScore: ', playerScore);
-  console.log('ComputerScore: ', computerScore);
+  input =  input.toLowerCase();
+  //checking to make sure input is valid
+  let check = validateInput(input);
+  if (check) {
+    console.log(input);
+  }
 }
 
-const playerSelection = 'RocK';
-const computerSelection = getComputerChoice();
-
-//console.log(playRound(playerSelection, computerSelection));
+function playRound () {
+  const playerSelection = playerChoice();
+  const computerSelection = getComputerChoice();
+}
 
 function game () {
-  //play five rounds of the game
-  for (let i = 0; i < 5; i++) {
-    playRound(playerSelection, computerSelection);
+  playRound();
+}
+
+//validating input function
+function validateInput(choice) {
+  if (choices.includes(choice)) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-console.log(game());
+game();
+
+// function playRound (playerSelection, computerSelection) {
+
+//   playerSelection = playerSelection.toLowerCase();
+//   console.log('Player: ', playerSelection);
+//   let playerScore = 0;
+//   computerSelection = computerSelection.toLowerCase();
+//   console.log('Computer: ', computerSelection);
+//   let computerScore = 0;
+//   if (playerSelection === 'rock' && computerSelection === 'paper') {
+//     computerScore++;
+//   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+//     computerScore++;
+//   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+//     computerScore++;
+//   } else if (playerSelection === computerSelection) {
+
+//   } else {
+//     playerScore++;
+//   }
+//   console.log('PlayerScore: ', playerScore);
+//   console.log('ComputerScore: ', computerScore);
+// }
+
+// const playerSelection = 'RocK';
+// const computerSelection = getComputerChoice();
+
+// //console.log(playRound(playerSelection, computerSelection));
+
+// function game () {
+//   //play five rounds of the game
+//   for (let i = 0; i < 5; i++) {
+//     playRound(playerSelection, computerSelection);
+//   }
+// }
+
+// console.log(game());
