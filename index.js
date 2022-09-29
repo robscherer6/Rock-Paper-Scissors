046e00f76a37c3957b859fe178ca5cd4ff54db00
Rob Scherer <robscherer6@gmail.com>
2022-09-28 20:19:02 -0400

    displaying score via functions for both players

--- index.js
+++ index.js
@@ -11,29 +11,66 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-// let pick;
-// const btn = document.querySelectorAll('button').forEach(button => {
-//   button.addEventListener('click', () => {
-//     pick = button.innerText;
-//     console.log('PICK: ', pick)
-//   });
-// })
 
 //play a single round of rps
-function playRound (round) {
-  // console.log(btn.innerText.toLowerCase());
-  const playerSelection = btn.innerText.toLowerCase();
-  const computerSelection = getComputerChoice();
-  //console.log('Computer: ', computerSelection);
-  const winner = determineWinner(playerSelection, computerSelection);
-  //console.log('Winner: ', winner);
+function playRound (playerChoice) {
+  let wins = checkWins();
+  if (wins >= 5) {
+    return;
+  }
+  const computerChoice = getComputerChoice();
+
+  const winner = determineWinner(playerChoice, computerChoice);
+
   winners.push(winner);
-  logRound(playerSelection, computerSelection, winner, round);
+  tallyWins();
+  displayRound(playerChoice, computerChoice, winner);
+  wins = checkWins();
+  if (wins === 5) {
+    displayEnd();
+  }
 }
 
-function game () {
-  playRound();
-  logWins();
+function displayEnd() {
+  let playersWins = winners.filter(item => item === 'Player').length;
+  if (playerWins === 5) {
+    document.querySelector('.winner').textContent = 'You won 5 times!!!';
+  } else {
+    document.querySelector('.winner').textContent = 'Sorry, the computer won 5 times!!!';
+  }
+  document.querySelector('.reset').style.display = 'flex'
+}
+
+function displayRound(playerChoice, computerChoice, winner) {
+  document.querySelector('.playerChoice').textContent = `You Chose: ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)}`;
+  document.querySelector('.computerChoice').textContent = `The Computer Chose: ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`;
+  document.querySelector('.winner').textContent = `Winner: ${winner}`;
+}
+
+function tallyWins() {
+  let playerWins = winners.filter(item => item === 'Player').length;
+  let computerWins = winners.filter(item => item === 'Computer').length;
+  let ties = winners.filter(item => item === 'Tie').length;
+  document.querySelector('.playerScore').textContent = `Score: ${playerWins}`;
+  document.querySelector('.computerScore').textContent = `Score: ${computerWins}`;
+  document.querySelector('.ties').textContent = `Ties: ${ties}`;
+}
+
+function startGame () {
+  let imgs = document.querySelectorAll('img');
+  imgs.forEach((img) => {
+    img.addEventListener(('click'), () => {
+      if (img.id) {
+        playRound(img.id);
+      }
+    })
+  })
+}
+
+function checkWins () {
+  let playerWins = winners.filter(item => item === 'Player').length;
+  let computerWins = winners.filter(item => item === 'Computer').length;
+  return Math.max(playerWins, computerWins);
 }
 
 function determineWinner (choiceP, choiceC) {
@@ -48,25 +85,13 @@ function determineWinner (choiceP, choiceC) {
   }
 }
 
-function logWins() {
+function setWins() {
   let playerWins = winners.filter(item => item === 'Player').length;
   let computerWins = winners.filter(item => item === 'Computer').length;
   let ties = winners.filter(item => item === 'Tie').length;
-
-  if (playerWins > computerWins) {
-    alert('You win!!!');
-  } else if (playerWins < computerWins) {
-    alert('The Computer wins, better luck next time teeHee.')
-  } else {
-    alert('Its a tie, please play again...')
-  }
 }
 
-function logRound(playerChoice, computerChoice, winner, round) {
-  console.log('Round: ', round);
-  console.log('Player Chose: ', playerChoice);
-  console.log('Computer Chose: ', computerChoice);
-  console.log(winner, 'won the round');
-  console.log('-------------------------------')
-}
+startGame();
+
+
 
